5fd8a1ef4b3a99217be12f55d70f145d30dd0ac4
Cristhian Melo <rxtsel@outlook.com>
2025-06-27 21:50:50 -0500

    feat: add gammastep config

---
+++ home/linux/packages/gammastep.nix
@@ -0,0 +1,14 @@
+{
+  services.gammastep = {
+    enable = true;
+    latitude = 1.2033996035846188;
+    longitude = 77.27524860954428;
+
+    settings = {
+      general = {
+        fade = 1;
+        adjustment-method = "wayland";
+      };
+    };
+  };
+}
