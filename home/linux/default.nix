6c5cb5dfa35a04c40f850afcaf890e5b8ced7d4e
Cristhian Melo <rxtsel@outlook.com>
2025-06-30 15:00:29 -0500

    feat(deps): add rustup

--- home/linux/default.nix
+++ home/linux/default.nix
@@ -35,6 +35,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    rustup
     nautilus
     discord
     appimage-run
