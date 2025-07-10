850654e09cc3121a5bd801adf647c725610bb200
Cristhian Melo <rxtsel@outlook.com>
2025-07-10 03:24:32 -0500

    refactor(hypr): remove comments and unused files

--- .config/hypr/gtk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-gnome_schema="org.gnome.desktop.interface"
-gsettings set "$gnome_schema" font-name "Noto Sans"
-gsettings set "$gnome_schema" color-scheme "prefer-dark"
-gsettings set "$gnome_schema" icon-theme "WhiteSur-dark"
-gsettings set "$gnome_schema" cursor-theme "McMojave-cursors"
