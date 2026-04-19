ab53aa3de5da35992081111771d3e50300dc04ac
Cristhian Melo <rxtsel@outlook.com>
2026-04-18 21:22:40 -0500

    chore(git): add more config for git

--- modules/home/programs/cli/git.nix
+++ modules/home/programs/cli/git.nix
@@ -16,6 +16,20 @@
           email = user.email;
           signingkey = user.gitSigningKeyPath;
         };
+
+        init.defaultBranch = "main";
+        pull.rebase = false;
+        push.autoSetupRemote = true;
+
+        alias = {
+          a = "add";
+          s = "status -sb";
+          c = "commit -m";
+          l = "log --pretty=format:\"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)\" --graph --date=relative --decorate --all";
+        };
+
+        gpg.format = "ssh";
+        commit.gpgSign = true;
       };
     };
   };
