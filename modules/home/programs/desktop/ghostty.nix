db5c3828e127885599328875d8cc57fb701f5360
Cristhian Melo <rxtsel@outlook.com>
2026-04-20 00:04:31 -0500

    feat(ghostty): add `solarized-osaka` theme colors

--- modules/home/programs/desktop/ghostty.nix
+++ modules/home/programs/desktop/ghostty.nix
@@ -6,7 +6,7 @@
       installVimSyntax = true;
 
       settings = {
-        theme = "solarized";
+        theme = "light:solarized-osaka-light, dark:solarized-osaka-dark";
         background-opacity = 0.5;
         font-family = "CaskaydiaCove Nerd Font";
         font-size = 18;
@@ -43,6 +43,59 @@
           "alt+t>h=previous_tab"
           "alt+t>o=toggle_tab_overview"
         ];
+      };
+
+      themes = {
+        solarized-osaka-dark = {
+          background = "001419";
+          foreground = "839395";
+          cursor-color = "839395";
+          selection-background = "1a6397";
+          selection-foreground = "839395";
+          palette = [
+            "0=#001419"
+            "1=#db302d"
+            "2=#849900"
+            "3=#b28500"
+            "4=#268bd3"
+            "5=#d23681"
+            "6=#29a298"
+            "7=#fdf5e2"
+            "8=#063540"
+            "9=#f55350"
+            "10=#b7f900"
+            "11=#ffbf00"
+            "12=#46acf5"
+            "13=#f254a0"
+            "14=#2aeddd"
+            "15=#ffffff"
+          ];
+        };
+        solarized-osaka-light = {
+          background = "fdf5e2";
+          foreground = "576d74";
+          cursor-color = "576d74";
+          selection-background = "46acf5";
+          selection-foreground = "576d74";
+          palette = [
+            "0=#ffffff"
+            "1=#db302d"
+            "2=#849900"
+            "3=#b28500"
+            "4=#268bd3"
+            "5=#d23681"
+            "6=#29a298"
+            "7=#002c38"
+            "8=#ede7d3"
+            "9=#b7211f"
+            "10=#586600"
+            "11=#664c00"
+            "12=#1a6397"
+            "13=#af2668"
+            "14=#1a6265"
+            "15=#001419"
+          ];
+        };
       };
     };
   };
