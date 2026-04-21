44baf9827bb96176f22a46acc40d8aed24b4b73b
Cristhian Melo <rxtsel@outlook.com>
2026-04-20 19:15:31 -0500

    chore(fcitx5): remove addons

--- modules/nixos/services/fcitx5.nix
+++ modules/nixos/services/fcitx5.nix
@@ -1,15 +1,10 @@
 {
-  flake.modules.nixos.fcitx5 = {pkgs, ...}: {
+  flake.modules.nixos.fcitx5 = {...}: {
     i18n.inputMethod = {
       enable = true;
       type = "fcitx5";
 
       fcitx5 = {
-        addons = with pkgs; [
-          fcitx5-gtk
-          qt6Packages.fcitx5-configtool
-        ];
-
         waylandFrontend = true;
       };
     };
