56aa44b2b13526f33868cf931cb3c7b45d758b54
Cristhian Melo <rxtsel@outlook.com>
2026-07-28 15:23:34 -0500

    chore(host): code style format

--- modules/hosts/thinkpad-t14/hardware.nix
+++ modules/hosts/thinkpad-t14/hardware.nix
@@ -5,31 +5,29 @@
     modulesPath,
     ...
   }: {
-    imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    imports = [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "sdhci_pci" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+    boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "sdhci_pci"];
+    boot.initrd.kernelModules = [];
+    boot.kernelModules = ["kvm-intel"];
+    boot.extraModulePackages = [];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/4119a1ac-ef9a-4153-a8dc-1ed4e24f37c2";
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/4119a1ac-ef9a-4153-a8dc-1ed4e24f37c2";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/9B28-BB45";
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/9B28-BB45";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = ["fmask=0022" "dmask=0022"];
     };
 
-    swapDevices = [
-      {device = "/swapfile";}
-    ];
+    swapDevices = [];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
 }
